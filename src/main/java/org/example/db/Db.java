520a7f18cf851b3b5cfe793f83da04a18c4c386e
muminova_02 <muminova.2m@gmail.com>
2024-06-24 22:41:14 +0500

    image path fixed

--- src/main/java/org/example/db/Db.java
+++ src/main/java/org/example/db/Db.java
@@ -86,25 +86,25 @@ public class Db {
             db=new Db();
 //            User user = new User(1121l,"Muqad","+889984");
 //            db.users.add(user)
-            db.getAllMeals().put(new MenuType("Ichimlik","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMenu\\ichimliklar.jpg"),new ArrayList<>());
+            db.getAllMeals().put(new MenuType("Ichimlik","src/main/resources/imageMenu/ichimliklar.jpg"),new ArrayList<>());
             db.getAllMeals().put(new MenuType("Salatlar","photo"),new ArrayList<>());
-            db.getAllMeals().put(new MenuType("Ovqatlar","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMenu\\ovqatlar.jpg"),new ArrayList<>());
+            db.getAllMeals().put(new MenuType("Ovqatlar","src/main/resources/imageMenu/ovqatlar.jpg"),new ArrayList<>());
             db.getAllMeals().put(new MenuType("Muzqaymoqlar","photo"),new ArrayList<>());
             db.getAllMeals().put(new MenuType("Burgers","photo"),new ArrayList<>());
             db.getAllMeals().forEach((menuType, meals) -> {
                 if (menuType.getTitle().equals("Ichimlik")){
-                    meals.add(new Meal("Kola","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ichimliklar\\cola.png"," 1 litrli",10000d));
-                    meals.add(new Meal("Pepsi","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ichimliklar\\Pepsi.jpg","1 litrli",12000d));
-                    meals.add(new Meal("Dinay","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ichimliklar\\Dinay.jpg","1 litrli",9000d));
-                    meals.add(new Meal("Fanta","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ichimliklar\\fanta.jpg","G1 litrli",10000d));
-                    meals.add(new Meal("Maxito","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ichimliklar\\Moxito.jpg","0.5 banchniy",13000d));
+                    meals.add(new Meal("Kola","src/main/resources/imageMeal/ichimliklar/cola.png"," 1 litrli",10000d));
+                    meals.add(new Meal("Pepsi","src/main/resources/imageMeal/ichimliklar/Pepsi.jpg","1 litrli",12000d));
+                    meals.add(new Meal("Dinay","src/main/resources/imageMeal/ichimliklar/Dinay.jpg","1 litrli",9000d));
+                    meals.add(new Meal("Fanta","src/main/resources/imageMeal/ichimliklar/fanta.jpg","G1 litrli",10000d));
+                    meals.add(new Meal("Maxito","src/main/resources/imageMeal/ichimliklar/Moxito.jpg","0.5 banchniy",13000d));
                 }else if(menuType.getTitle().equals("Ovqatlar")){
-                    meals.add(new Meal("Sho'rva","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ovqatlar\\Sho'rva.jpg","Toshkent shorva pors",35000d));
-                    meals.add(new Meal("Palov","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ovqatlar\\Palov.jpg","Samarqand oshi pors",40000d));
-                    meals.add(new Meal("Mastava","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ovqatlar\\mastava.jpg","Naxorgi mastava pors",30000d));
-                    meals.add(new Meal("Besh barmoq","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ovqatlar\\beshbarmoq.jpg","Salat qoshimchasi bilan pors",45000d));
-                    meals.add(new Meal("Chuchvara","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ovqatlar\\chuchvara.jpg","Mazali chuchvara pors",23000d));
-                    meals.add(new Meal("Qotirma","E:\\JAVA lesson pdp\\Restaurant\\src\\main\\resources\\imageMeal\\ovqatlar\\qotirma.jpg","200 gr gosht 100 gr kartoshka pors",38000d));
+                    meals.add(new Meal("Sho'rva","src/main/resources/imageMeal/ovqatlar/Sho'rva.jpg","Toshkent shorva pors",35000d));
+                    meals.add(new Meal("Palov","src/main/resources/imageMeal/ovqatlar/Palov.jpg","Samarqand oshi pors",40000d));
+                    meals.add(new Meal("Mastava","src/main/resources/imageMeal/ovqatlar/mastava.jpg","Naxorgi mastava pors",30000d));
+                    meals.add(new Meal("Besh barmoq","src/main/resources/imageMeal/ovqatlar/beshbarmoq.jpg","Salat qoshimchasi bilan pors",45000d));
+                    meals.add(new Meal("Chuchvara","src/main/resources/imageMeal/ovqatlar/chuchvara.jpg","Mazali chuchvara pors",23000d));
+                    meals.add(new Meal("Qotirma","src/main/resources/imageMeal/ovqatlar/qotirma.jpg","200 gr gosht 100 gr kartoshka pors",38000d));
                 }else if(menuType.getTitle().equals("Salatlar")){
                     meals.add(new Meal("Sezar","PhotoMeal","Mayanezli salat",12000d));
                     meals.add(new Meal("aliviya","PhotoMeal","Mayanezli salat ",13000d));
